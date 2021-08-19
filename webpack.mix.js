dcc00577176bdd54f59af78c9e281617224ffc0b
Anatoly Silko <silko.a@gmail.com>
2021-08-19 12:07:23 +0100

    Laravel Mix Build

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,8 @@ const mix = require('laravel-mix');
 
 mix.disableSuccessNotifications();
 
-mix.less('./src/app.less', './elements.min.css');
+if (mix.inProduction()) {
+    mix.less('./src/app.less', './elements.min.css');
+} else {
+    mix.less('./src/app.less', './elements.css');
+}
