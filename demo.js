a0627505f932f70073d9a2f3e76f61e0ae7d3ebc
Anatoly Silko <silko.a@gmail.com>
2021-08-21 13:45:13 +0100

    Demo Improvements & CSS Fixes

--- demo.js
+++ demo.js
@@ -1,14 +1,50 @@
 $(document).ready(function () {
+
+    // Making all the <code> elements visible (with no JS they make no sense)
+    // And wrap them into the .relative div to add a copy button
+    $("code").show().wrap('<div class="relative"></div>');
+    $(".relative code").after('<button class="button button_transparent far fa-copy"></button>');
+
+    // Outputting the source code inside each <code> block
     $("code").each(function () {
 
+        // The original
         var code = $(this).html();
 
+        // Measuring the initial indent
         var shift = code.match(/^\s{0,100}/)[0].length;
-        console.log(shift);
 
-        var replaced = code.trim().replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '<br>').replace(/\s/g, '&nbsp;');
+        // Replacing that shift
+        var regex = new RegExp("\\s{" + shift + "}", "g");
+
+        // Replacing the < and >
+        var replaced = code.trim().replace(/</g, '&lt;').replace(/>/g, '&gt;');
+
+        if (shift > 0) {
+            replaced = replaced.replace(regex, '<br>');
+        }
+
+        replaced = replaced.replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '<br>').replace(/\s/g, '&nbsp;');
 
         $(this).html(replaced);
+
+    });
+
+    $(".relative code + .button").click(function (e) {
+
+        e.preventDefault();
+
+        // The button we just clicked on
+        var button = $(this);
+
+        // The code we're about to copy
+        var text = button.prev('code').html();
+
+        navigator.clipboard.writeText(text).then(function() {
+            button.addClass('green').removeClass('far fa-copy').addClass('fa-check fas');
+        }, function(err) {
+            button.addClass('red').removeClass('far fa-copy').addClass('fa-times fas');
+        });
 
     });
 
